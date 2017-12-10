8e143fcebbcd81bbfed554d9bcbe7909349a1b94
lake <102535481@qq.com>
2017-12-10 13:54:10 +0800

    add third impl method

--- src/screens/MyHomeScreen.js
+++ src/screens/MyHomeScreen.js
@@ -1,7 +1,22 @@
 import React from 'react';
-import {Image,Text,View,Button,StyleSheet} from 'react-native';
+import {Image,Text,View,Button,StyleSheet,DeviceEventEmitter} from 'react-native';
 import {TabNavigator,NavigationActions} from 'react-navigation';
 import Stacks from './Stacks';
+
+function getCurrentRouteName(navigationState) {
+    if (!navigationState) {
+        return null;
+    }
+    const route = navigationState.routes[navigationState.index];
+    if (route.routes) {
+        return getCurrentRouteName(route);
+    }
+    return route.routeName;
+}
+
+function checkTabBarVisible(params) {
+    return (params && params.tabBarVisible != null) ? !params.tabBarVisible : true
+}
 
 export default class MyHomeScreen extends React.Component {
     static navigationOptions = {
@@ -14,6 +29,16 @@ export default class MyHomeScreen extends React.Component {
             />
         ),
     };
+
+    componentDidMount() {
+        this.hideTabBarListener = DeviceEventEmitter.addListener('hideTabBar',function () {
+            this.buttonClick();
+        }.bind(this))
+    }
+
+    componentWillUnmount() {
+        this.hideTabBarListener.remove();
+    }
 
     buttonClick = ()=>{
         const setParamsAction = NavigationActions.setParams({
@@ -28,16 +53,16 @@ export default class MyHomeScreen extends React.Component {
             <View
                 style={{
                     flex:1,
+                    paddingTop:30,
                     justifyContent:'center'
                 }}
             >
                 <Button
                     onPress={this.buttonClick}
-                    title="hideTabBar"
+                    title="方法1"
                 />
                 <Stacks screenProps={{parentNavigation:this.props.navigation}} />
             </View>
-
         );
     }
 }
--- src/screens/Stacks.js
+++ src/screens/Stacks.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View,Text,Button} from 'react-native';
+import {View,Text,Button,DeviceEventEmitter} from 'react-native';
 import {NavigationActions, StackNavigator} from 'react-navigation';
 
 class S1 extends React.Component{
@@ -12,11 +12,47 @@ class S1 extends React.Component{
         this.props.screenProps.parentNavigation.dispatch(setParamsAction);
     };
 
+    buttonClick2 = ()=>{
+        DeviceEventEmitter.emit('hideTabBar');//通过事件方式
+    };
+
     render(){
         return <View>
             <Button
                 onPress={this.buttonClick}
-                title="childStackHideTabBar"
+                title="方法2"
+            />
+            <Button
+                onPress={this.buttonClick2}
+                title="方法3"
+            />
+        </View>
+    }
+}
+
+class S2 extends React.Component{
+
+    buttonClick = ()=>{
+        const setParamsAction = NavigationActions.setParams({
+            params: { hideTabBar: true },
+            key: 'Home'
+        });
+        this.props.screenProps.parentNavigation.dispatch(setParamsAction);
+    };
+
+    buttonClick2 = ()=>{
+        DeviceEventEmitter.emit('hideTabBar');//通过事件方式
+    };
+
+    render(){
+        return <View>
+            <Button
+                onPress={this.buttonClick}
+                title="方法2"
+            />
+            <Button
+                onPress={this.buttonClick2}
+                title="方法3"
             />
         </View>
     }
@@ -25,5 +61,8 @@ class S1 extends React.Component{
 export default StackNavigator({
     S1: {
         screen: S1,
+    },
+    S2: {
+        screen: S2,
     },
 });
